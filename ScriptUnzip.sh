a75eb4c7ed3ed6b88f86251d83b6a7ccdc0e6a77
thocayla <thomas.cayla@eleve.ensai.fr>
2017-01-04 10:31:29 +0100

    Add files via upload

---
+++ ScriptUnzip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unzip ./Prix2016-1204.zip -d /home/thomas/Documents/ITToolsforBigData/Project
+
+unzip ./Prix2015.zip -d /home/thomas/Documents/ITToolsforBigData/Project
+
+unzip ./Prix2014.zip -d /home/thomas/Documents/ITToolsforBigData/Project
+
+unzip ./Prix2013.zip -d /home/thomas/Documents/ITToolsforBigData/Project
+
+unzip ./PrixCarburants_quotidien_20161216.zip -d /home/thomas/Documents/ITToolsforBigData/Project
+
+#End
